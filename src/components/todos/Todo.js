18d984ded67c2a0153814d681146173a7f4dfd06
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-03-05 20:49:36 -0500

    Done.

---
+++ src/components/todos/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Todo from './Todo'
+ 
+class TodosContainer extends Component {
+ 
+  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+ 
+  render() {
+    return(
+      <div>
+        {this.renderTodos()}
+      </div>
+    );
+  }
+};
+ 
+export default Todo;
